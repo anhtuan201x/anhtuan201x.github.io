eb10030a83ec3f839bba1a2be31825bd503de4b6
anhtuan2016 <anhtuan20160915@gmail.com>
2026-09-20 14:07:18 +0700

    Update sync.sh

--- sync.sh
+++ sync.sh
@@ -4,66 +4,18 @@ mkdir -p ipas
 
 if [ -f "Release" ]; then sed -i 's/\r$//' Release; fi
 
-if [ -f "debs/Payload.zip.001" ] && [ -f "debs/Payload.zip.004" ]; then
-    cat debs/Payload.zip.* > debs/Payload.zip
-    unzip -q debs/Payload.zip -d debs/
+if [ -d "debs/Payload" ]; then
+    cd debs
+    zip -qyX -r cargame.ipa Payload
+    cd ..
     mv debs/cargame.ipa ipas/cargame.ipa
-    cp ipas/cargame.ipa debs/cargame.ipa
-    rm -f debs/Payload.zip*
+    rm -rf debs/Payload
 fi
 
 for ipa in debs/*.ipa; do
     if [ -f "$ipa" ]; then
         filename=$(basename -- "$ipa")
-        appname="${filename%.*}"
-        clean_name=$(echo "$appname" | tr -cd '[:alnum:]' | tr '[:upper:]' '[:lower:]')
-        
-        rm -rf debs/tmp_ipa debs/tmp_out
-        mkdir -p debs/tmp_ipa
-        mkdir -p debs/tmp_out/DEBIAN
-        mkdir -p debs/tmp_out/Applications
-        
-        unzip -q "$ipa" -d debs/tmp_ipa
-        
-        app_folder=$(find debs/tmp_ipa/Payload -maxdepth 2 -name "*.app" | head -n 1)
-        if [ -d "$app_folder" ]; then
-            cp -r "$app_folder" debs/tmp_out/Applications/
-            
-            infoplist="debs/tmp_out/Applications/$(basename "$app_folder")/Info.plist"
-            bid="com.anhtuan201x.$clean_name"
-            ver="1.0"
-            
-            if [ -f "$infoplist" ]; then
-                extracted_bid=$(grep -A 1 "CFBundleIdentifier" "$infoplist" | grep "<string>" | sed 's/.*<string>\(.*\)<\/string>.*/\1/' | tr -cd '[:alnum:]._-')
-                extracted_ver=$(grep -A 1 "CFBundleVersion" "$infoplist" | grep "<string>" | sed 's/.*<string>\(.*\)<\/string>.*/\1/' | tr -cd '[:alnum:]._-')
-                if [ ! -z "$extracted_bid" ]; then bid="$extracted_bid"; fi
-                if [ ! -z "$extracted_ver" ]; then ver="$extracted_ver"; fi
-                sed -i '/<dict>/a \    <key>UIPrerenderedIcon<\/key>\n    <true\/>' "$infoplist"
-            fi
-            
-            cat << 'EOF' > debs/tmp_out/DEBIAN/control
-Package: com.anhtuan201x.placeholder
-Name: Placeholder
-Version: 1.0
-Architecture: iphoneos-arm
-Maintainer: AnhTuan201X <anhtuan201x@github.io>
-Section: Applications
-Description: Cydia Application
-EOF
-            
-            sed -i "s/^Package:.*/Package: $bid/" debs/tmp_out/DEBIAN/control
-            sed -i "s/^Name:.*/Name: $clean_name/" debs/tmp_out/DEBIAN/control
-            sed -i "s/^Version:.*/Version: $ver/" debs/tmp_out/DEBIAN/control
-            sed -i "s/^Description:.*/Description: Ung dung duoc bien doi tu dong tu file IPA sang DEB boi AnhTuan201X Bot./" debs/tmp_out/DEBIAN/control
-            
-            sed -i 's/\r$//' debs/tmp_out/DEBIAN/control
-            find debs/tmp_out -type f -exec sed -i 's/\r$//' {} +
-            chmod -R 0755 debs/tmp_out
-            chmod 0644 debs/tmp_out/DEBIAN/control
-            
-            dpkg-deb --option Uniform-Compression=no -Zgzip --format=2.0 --build debs/tmp_out debs/cargame.deb
-        fi
-        rm -f "debs/$filename"
+        mv "$ipa" ipas/
     fi
 done
 
