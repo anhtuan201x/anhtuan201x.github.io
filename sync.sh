d920797f0c0c59a0a6e7fa8fd60fd01282daee32
anhtuan2016 <anhtuan20160915@gmail.com>
2026-09-20 14:17:33 +0700

    Update sync.sh

--- sync.sh
+++ sync.sh
@@ -4,18 +4,15 @@ mkdir -p ipas
 
 if [ -f "Release" ]; then sed -i 's/\r$//' Release; fi
 
-if [ -f "debs/Payload.zip.001" ] && [ -f "debs/Payload.zip.004" ]; then
-    cat debs/Payload.zip.* > debs/Payload.zip
-    unzip -q debs/Payload.zip -d debs/
+if [ -d "debs/Payload" ]; then
+    cd debs
+    zip -qyX -r cargame.ipa Payload
+    cd ..
     
-    if [ -d "debs/Payload" ]; then
-        cd debs
-        zip -qyX -r cargame.ipa Payload
-        cd ..
+    if [ -f "debs/cargame.ipa" ]; then
         mv debs/cargame.ipa ipas/cargame.ipa
+        rm -rf debs/Payload
     fi
-    rm -rf debs/Payload debs/Payload.zip
-    rm -f debs/Payload.zip.*
 fi
 
 for ipa in debs/*.ipa; do
