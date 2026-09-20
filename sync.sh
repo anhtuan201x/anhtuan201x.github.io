9489fd741b3a10ddbfef1c7c11c9b55fc5401de3
anhtuan2016 <anhtuan20160915@gmail.com>
2026-09-20 12:07:58 +0700

    Update sync.sh

--- sync.sh
+++ sync.sh
@@ -61,19 +61,22 @@ done
 
 rm -rf debs/tmp_icons
 mkdir -p debs/tmp_icons/DEBIAN
-mkdir -p debs/tmp_icons/usr/share/cydia/sections
+mkdir -p debs/tmp_icons/Applications/Cydia.app/Sections
+
 if [ -f "CydiaIcon.png" ]; then
-    cp CydiaIcon.png debs/tmp_icons/usr/share/cydia/sections/com.anhtuan201x.repoicons.png
+    cp CydiaIcon.png debs/tmp_icons/Applications/Cydia.app/Sections/com.anhtuan201x.repoicons.png
 fi
+
 cat << 'EOF' > debs/tmp_icons/DEBIAN/control
 Package: com.anhtuan201x.repoicons
 Name: AnhTuan201X Repo Icons
 Version: 1.0
 Architecture: iphoneos-arm
 Maintainer: AnhTuan201X <anhtuan201x@github.io>
-Section: Themes
-Description: Bo suu tap bieu tuong logo doc quyen giup hien thi anh nho cho toan bo tweak trong nguon cua Anh Tuan.
+Section: System
+Description: Goi bo sung cau truc hinh anh bieu tuong nguon truc tiep vao bo nao Cydia he thong.
 EOF
+
 sed -i 's/\r$//' debs/tmp_icons/DEBIAN/control
 find debs/tmp_icons -type f -exec sed -i 's/\r$//' {} +
 chmod -R 0755 debs/tmp_icons
@@ -85,7 +88,7 @@ rm -f Packages Packages.bz2
 dpkg-scanpackages -m debs /dev/null > Packages
 sed -i 's/\r$//' Packages
 
-sed -i '/^Description:/i \Icon: https://anhtuan201x.github.io/CydiaIcon.png' Packages
+sed -i '/^Description:/i \Icon: http://anhtuan201x.github.io/CydiaIcon.png' Packages
 
 bzip2 -fk Packages
 
